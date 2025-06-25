b7c926ff33b395f15b6b51e3ec3f7ec558e814d2
dilara2016 <m.123.hunain@gmail.com>
2025-06-25 12:36:56 +0200

    a2

---
+++ a2.py
@@ -0,0 +1,52 @@
+from _collections import deque
+
+class stack:
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+
+    def push(self,x):
+        self.q1.append(x)
+
+
+    def pop(self):
+        if(not self.q1):
+            return
+        while(len(self.q1)!= 1):
+            self.q2.append(self.q1.popleft())
+        self.q1, self.q2 = self.q2, self.q1
+
+
+    def top(self):
+        if(not self.q1):
+            return
+        while(len(self.q1)!=1):
+            self.q2.append(self.q1.popleft())
+
+        top = self.q1[0]
+        self.q2.append(self.q1.popleft())
+
+        self.q1, self.q2 = self.q2, self.q1
+
+        return top
+    
+
+    def size(self):
+        return len(self.q1)
+    
+
+if __name__ == '__main__':
+    s = stack()
+    s.push(1)
+    s.push(2)
+    s.push(3)
+
+    print("Current size: ", s.size())
+    print(s.top())
+    s.pop()
+    print(s.top())
+    s.pop()
+    print(s.top())
+
+    print("Current size: ", s.size())
